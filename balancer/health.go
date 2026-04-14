b57fd415fe7cf2974841a63aa9bb490c6637eb2e
SubProblem <gukag6@gmail.com>
2026-04-15 00:20:59 +0400

    run health check immediately on startup to avoid dropping connections in first interval

--- balancer/health.go
+++ balancer/health.go
@@ -7,21 +7,26 @@ import (
 )
 
 func StartHealthChecker(backends []Backend, interval time.Duration) {
+	checkAll(backends)
 	ticker := time.NewTicker(interval)
 
 	go func() {
 		defer ticker.Stop()
 		for range ticker.C {
-			for i := range backends {
-				conn, err := net.DialTimeout("tcp", backends[i].Address, 2*time.Second)
-				if err != nil {
-					log.Printf("Health check failed for %s: %v", backends[i].Address, err)
-					backends[i].Healthy.Store(false)
-				} else {
-					conn.Close()
-					backends[i].Healthy.Store(true)
-				}
-			}
+			checkAll(backends)
 		}
 	}()
+}
+
+func checkAll(backends []Backend) {
+	for i := range backends {
+		conn, err := net.DialTimeout("tcp", backends[i].Address, 2*time.Second)
+		if err != nil {
+			log.Printf("Health check failed for %s: %v", backends[i].Address, err)
+			backends[i].Healthy.Store(false)
+		} else {
+			conn.Close()
+			backends[i].Healthy.Store(true)
+		}
+	}
 }
